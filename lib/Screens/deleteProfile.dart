a3821bb688803213fafd7d0ebc1b6f825cb54d0b
shreejanmdr <shreejan0497@gmail.com>
2023-07-11 19:55:52 +0545

    snackbar and clear feild removed

--- lib/Screens/deleteProfile.dart
+++ lib/Screens/deleteProfile.dart
@@ -316,30 +316,6 @@ class DeleteAccount extends StatelessWidget {
 
 
 
-  void _showSnackBar(BuildContext context, String message) {
-
-    ScaffoldMessenger.of(context).showSnackBar(
-
-      SnackBar(
-
-        content: Text(message),
-
-      ),
-
-    );
-
-  }
-
-
-
-
-  void _clearFields() {
-
-    email.clear();
-
-    pass.clear();
-
-  }
 
 }
 
