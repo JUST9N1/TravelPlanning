2389fbb5ee4234939a553456fd259b7318b53c48
shreejanmdr <shreejan0497@gmail.com>
2023-07-14 01:11:07 +0545

    adding code

--- lib/Screens/payment.dart
+++ lib/Screens/payment.dart
@@ -13,3 +13,59 @@ class Payment extends StatefulWidget {
 
 }
 
+class _PaymentState extends State<Payment> {
+
+  int selectedPaymentOption = 0; // Track the selected payment option
+
+  bool paymentSuccessful = false; // Track payment success
+
+
+
+
+  @override
+
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+
+        body: Container(
+
+        width: double.infinity,
+
+        color: Colors.white,
+
+        child: Column(
+
+        crossAxisAlignment: CrossAxisAlignment.center,
+
+        children: [
+
+        Container(
+
+        padding: const EdgeInsets.fromLTRB(14, 21, 14, 118),
+
+    width: double.infinity,
+
+    child: Column(
+
+    crossAxisAlignment: CrossAxisAlignment.start,
+
+    children: [
+
+    Container(
+
+    padding: const EdgeInsets.only(left: 110, top: 50),
+
+    margin: const EdgeInsets.only(bottom: 94),
+
+    child: const Text(
+
+    'Payment',
+
+    textAlign: TextAlign.right,
+
+    style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+
+    ),
+
+    ),
