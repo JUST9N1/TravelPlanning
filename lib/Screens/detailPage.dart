450a635d3b61d878bbc904d718ef52941bef6541
Sujan Gautam <susangautam062@gmail.com>
2023-08-11 20:11:11 +0545

    some changes to detailpages

--- lib/Screens/detailPage.dart
+++ lib/Screens/detailPage.dart
@@ -1,5 +1,5 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/material.dart';
+import  'package:cloud_firestore/cloud_firestore.dart';
+import  'package:flutter/material.dart';
 import 'package:travelplanning/Screens/bookingPage.dart';
 import 'package:travelplanning/Screens/navpages/favoritePage.dart';
 
@@ -88,7 +88,7 @@ class _DetailPageState extends State<DetailPage> {
                           topRight: Radius.circular(20),
                         ),
                       ),
-                      // Dynamic
+
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
